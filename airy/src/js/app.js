6863e50d604064f2467f31a6bbb7f4ebfed9f075
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-10-17 23:06:22 +0400

    Fixed bug in definition of routes

--- airy/src/js/app.js
+++ airy/src/js/app.js
@@ -17,7 +17,6 @@ airyApp.config(['$routeProvider',
         $routeProvider
             .when('/login', {
                 templateUrl: 'static/partials/login.html',
-                controller: 'LoginController',
                 name: 'login',
                 resolve: {
                     user: function ($q, $location, airyUser) {
@@ -36,27 +35,22 @@ airyApp.config(['$routeProvider',
             })
             .when('/clients', {
                 templateUrl: 'static/partials/clients.html',
-                controller: 'ClientListController',
                 name: 'client_list'
             })
             .when('/clients/:clientId', {
                 templateUrl: 'static/partials/client.html',
-                contoller: 'ClientDetailController',
                 name: 'client_detail'
             })
             .when('/projects/:projectId', {
                 templateUrl: 'static/partials/project.html',
-                contorller: 'ProjectDetailController',
                 name: 'project_detail'
             })
             .when('/projects/:projectId/report', {
                 templateUrl: 'static/partials/report.html',
-                controller: 'ProjectReportController',
                 name: 'project_report'
             })
             .when('/reports', {
                 templateUrl: 'static/partials/reports.html',
-                controller: 'ReportListController',
                 name: 'report_list'
             })
             .otherwise({
