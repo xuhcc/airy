01ae9219c936d1ff3fa82eb20221305706fd746f
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-10-21 02:12:42 +0400

    Added tests for LoginController

--- jasmine/test_spec.js
+++ jasmine/test_spec.js
@@ -1,3 +1,37 @@
+describe('Controllers', function () {
+    'use strict';
+
+    var scope, ctrl;
+
+    beforeEach(module('airyControllers'));
+
+    describe('LoginController', function () {
+        var airyUser = {
+            login: function (password) {
+                this.password = password;
+            }
+        };
+
+        beforeEach(inject(function($controller, $rootScope){
+            scope = $rootScope.$new();
+            ctrl = $controller('LoginController', {
+                $scope: scope,
+                airyUser: airyUser
+            });
+        }));
+
+        it('should change the page title', function () {
+            expect(scope.title).toBe('Login');
+        });
+
+        it('should log in the user', function () {
+            scope.password = 'pwd';
+            scope.loginUser();
+            expect(airyUser.password).toBe('pwd');
+        });
+    });
+});
+
 describe('Filters', function () {
     'use strict';
 
@@ -13,4 +47,3 @@ describe('Filters', function () {
         expect(result).toBe('aaa<br>bbb');
     });
 });
-
