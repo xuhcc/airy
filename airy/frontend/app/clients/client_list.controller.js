a81e3e659ca537a57b0458c25932ff57135c0a81
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-12-22 22:37:55 +0300

    Rewrite client list controller using ES6

--- airy/frontend/app/clients/client_list.controller.js
+++ airy/frontend/app/clients/client_list.controller.js
@@ -1,58 +1,57 @@
-function ClientListController($rootScope, ngDialog, hotkeys, airyPopup, clientResource) {
-    var self = this;
-    self.clients = [];
-    self.createClient = createClient;
-    self.updateClient = updateClient;
-    self.deleteClient = deleteClient;
+class ClientListController {
 
-    fetchClients();
+    constructor($rootScope, ngDialog, hotkeys, airyPopup, clientResource) {
+        const self = this;
+        self.clients = [];
+        self.createClient = createClient;
+        self.updateClient = updateClient;
+        self.deleteClient = deleteClient;
 
-    $rootScope.title = 'Clients';
+        fetchClients();
 
-    hotkeys.add({
-        combo: 'alt+a',
-        callback: function (event) {
-            event.preventDefault();
-            createClient();
-        },
-    });
+        $rootScope.title = 'Clients';
 
-    function fetchClients() {
-        clientResource.list().success(function (data) {
-            self.clients = data.clients;
-        });
-    }
-
-    function createClient() {
-        ngDialog.open({
-            template: 'static/partials/client_form.html',
-            controller: 'ClientCreationController',
-            resolve: {
-                clients: function () {
-                    return self.clients;
-                },
+        hotkeys.add({
+            combo: 'alt+a',
+            callback: (event) => {
+                event.preventDefault();
+                createClient();
             },
         });
-    }
 
-    function updateClient(client) {
-        ngDialog.open({
-            template: 'static/partials/client_form.html',
-            controller: 'ClientUpdateController',
-            resolve: {
-                client: function () {
-                    return client;
-                },
-            },
-        });
-    }
-
-    function deleteClient(client) {
-        airyPopup.confirm('Delete client?', function () {
-            clientResource.delete(client).success(function (data) {
-                self.clients.splice(self.clients.indexOf(client), 1);
+        function fetchClients() {
+            clientResource.list().success((data) => {
+                self.clients = data.clients;
             });
-        });
+        }
+
+        function createClient() {
+            ngDialog.open({
+                template: 'static/partials/client_form.html',
+                controller: 'ClientCreationController',
+                resolve: {
+                    clients: () => self.clients,
+                },
+            });
+        }
+
+        function updateClient(client) {
+            ngDialog.open({
+                template: 'static/partials/client_form.html',
+                controller: 'ClientUpdateController',
+                resolve: {
+                    client: () => client,
+                },
+            });
+        }
+
+        function deleteClient(client) {
+            airyPopup.confirm('Delete client?', () => {
+                clientResource.delete(client).success((data) => {
+                    self.clients.splice(self.clients.indexOf(client), 1);
+                });
+            });
+        }
     }
 }
 
