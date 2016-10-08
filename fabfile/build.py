4641765d2552175c2c7e10b707e647ecb9a66b5e
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-10-08 15:33:30 +0300

    Use bower update command for building project

--- fabfile/build.py
+++ fabfile/build.py
@@ -28,12 +28,12 @@ def frontend(production=False):
     if production:
         env.run('npm install --production')
         env.run('npm rebuild node-sass')
-        env.run('npm run-script bower install --production')
+        env.run('npm run-script bower update --production')
         env.run('npm run-script grunt build:production')
     else:
         env.run('npm install')
         env.run('npm rebuild node-sass')
-        env.run('npm run-script bower install')
+        env.run('npm run-script bower update')
         env.run('npm run-script grunt build:development')
 
 
