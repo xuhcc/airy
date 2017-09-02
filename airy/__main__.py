48cd55cab25632b7b3e1f5512aa3bf2dd742ba2e
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-09-02 08:39:43 +0300

    Backend: Fix noqa statement in __main__

--- airy/__main__.py
+++ airy/__main__.py
@@ -3,6 +3,6 @@ import os.path
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from airy import runserver  # flake8: noqa
+from airy import runserver  # noqa: E402
 
 runserver()
