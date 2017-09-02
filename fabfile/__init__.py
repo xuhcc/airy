4a659912b13add7fc5a68b6dc385544f030be973
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-09-02 13:16:50 +0300

    Misc: Tidy up fabfile

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,13 +1,7 @@
-from fabric.api import env, local
+from fabric.api import env
 
-import build  # flake8: noqa
-import db  # flake8: noqa
-import check  # flake8: noqa
-try:
-    import deploy  # flake8: noqa
-except ImportError:
-    pass
+import build  # noqa
+import db  # noqa
+import check  # noqa
 
 env.use_ssh_config = True
-env.run = local
-env.editor = "nano"
--- fabfile/build.py
+++ fabfile/build.py
@@ -1,45 +1,29 @@
-from fabric.api import env, task, settings, prefix, local
+from fabric.api import task, prefix, local
 
 
 @task
 def dirs():
-    env.run("mkdir -p logs")
+    local('mkdir -p logs')
 
 
 @task
-def venv(production=False):
-    with settings(warn_only=True):
-        test_command = "test requirements.txt -ot venv/bin/activate"
-        if not production:
-            test_command += ' -a requirements_dev.txt -ot venv/bin/activate'
-        result = env.run(test_command)
-    if result.failed:
-        env.run("virtualenv venv")
-        with prefix(". venv/bin/activate"):
-            if production:
-                env.run("pip install -q -r requirements.txt --upgrade")
-            else:
-                env.run('pip install -r requirements_dev.txt --upgrade')
-        env.run("touch venv/bin/activate")
+def venv():
+    local('virtualenv -p /usr/bin/python3 venv')
+    with prefix('. venv/bin/activate'):
+        local('pip install -r requirements_dev.txt')
 
 
 @task
-def frontend(production=False):
-    if production:
-        env.run('npm install --production')
-        env.run('npm rebuild node-sass')
-        env.run('npm run-script bower update --production')
-        env.run('npm run-script grunt build:production')
-    else:
-        env.run('npm install')
-        env.run('npm rebuild node-sass')
-        env.run('npm run-script bower update')
-        env.run('npm run-script grunt build:development')
+def frontend():
+    local('npm install')
+    local('npm rebuild node-sass')
+    local('npm run-script bower update')
+    local('npm run-script grunt build:development')
 
 
 @task
 def watch():
-    env.run('npm run-script grunt watch')
+    local('npm run-script grunt watch')
 
 
 @task
@@ -49,7 +33,7 @@ def clean():
 
 
 @task(default=True)
-def all(production=False):
+def all():
     dirs()
-    venv(production=production)
-    frontend(production=production)
+    venv()
+    frontend()
