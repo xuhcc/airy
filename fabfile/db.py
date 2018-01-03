c41507c186236e8de7f83f6226f87342fd8d1058
Kirill Goncharov <kdgoncharov@yandex.ru>
2018-01-03 15:35:40 +0300

    Misc: Add fab commands for backing up and restoring dev database

--- fabfile/db.py
+++ fabfile/db.py
@@ -1,4 +1,6 @@
-from fabric.api import env, task, prefix, local
+from fabric.api import env, task, prefix, local, cd, sudo, puts
+
+from utils import vagrant
 
 
 @task
@@ -11,3 +13,19 @@ def migrate():
 def auto(message):
     with prefix('. venv/bin/activate'):
         local('alembic revision --autogenerate -m "{0}"'.format(message))
+
+
+@task
+def backup(database='airy'):
+    with vagrant(), cd('/vagrant/vagrant'):
+        sudo('pg_dump -Fc -U postgres {} > database.backup'.format(
+            database))
+        puts('Database backup created.')
+
+
+@task
+def restore(database='airy'):
+    with vagrant(), cd('/vagrant/vagrant'):
+        sudo('pg_restore -U postgres -d {} database.backup'.format(
+            database))
+        puts('Database restored.')
