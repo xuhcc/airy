3d2610055efd67a6704936569afd021a8f1b41c2
Kirill Goncharov <kdgoncharov@yandex.ru>
2018-05-08 01:10:43 +0200

    Frontend: Fix report test

--- frontend/app/reports/client_report.controller.spec.js
+++ frontend/app/reports/client_report.controller.spec.js
@@ -49,7 +49,7 @@ describe('Task report', function () {
         expect(ctrl.report).toBeDefined();
         expect(ctrl.client).toBeDefined();
         expect(ctrl.range).toBeDefined();
-        expect(ctrl.periods.length).toBe(4);
+        expect(ctrl.periods.length).toBe(5);
         expect(ctrl.period).toEqual(ctrl.periods[0]);
         spyOn(clientResourceMock, 'getReport').and.callThrough();
         scope.$digest();
